f3e3ec5c550f06a23c6abf4cf3ef52ee8872deba
AK4TRL <ak4trl@gmail.com>
2017-03-18 21:05:54 +0800

    update

--- Basic_Html/task_1_8/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp'),
-    less = require('gulp-less');
-
-gulp.task('testLess',function () {
-    gulp.src('less/styles.less')
-        .pipe(less())
-        .pipe(gulp.dest('css'))
-});
-
